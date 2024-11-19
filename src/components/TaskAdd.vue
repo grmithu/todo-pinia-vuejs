ebf118eae16ec02e572fe81caa3a6cda4a58a149
grmithu <golamrabbani3010@gmail.com>
2024-11-19 16:56:34 +0600

    To do design completed

---
+++ src/components/TaskAdd.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <form action="" class="mb-10 flex space-x-4">
+        <input type="text" name="" id="" placeholder="Add something.." class="w-full bg-gray-100 rounded border border-gray-300 outline-gray-300 py-1 px-3">
+        <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 hover:bg-indigo-600 rounded text-lg ">Add</button>
+    </form>
+
+</template>
